b42153f4092d83ba68a086e79c3c074a7012ed03
Ibimina hart <ibimina.c.hart@gmail.com>
2023-08-17 14:39:55 +0100

    feat: Individuals section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -57,10 +57,12 @@ const toggleMenuText = computed(() => {
         </div>
       </nav>
     </header>
-    <section class="relative pb-12 lg:pl-12 text-center px-4 mt-10 lg:flex lg:pr-0 lg:gap-8 lg:max-w-6xl ml-auto">
+    <section
+      class="relative pb-12 lg:pl-12 text-center px-4 mt-10 lg:flex lg:pr-0 lg:gap-8 lg:max-w-6xl ml-auto"
+    >
       <div class="max-w-lg">
         <h1 class="text-accent-black font-medium text-3xl mb-4 lg:text-6xl font-serif lg:text-left">
-          Run your <span class="text-accent-green  font-medium">payroll </span>
+          Run your <span class="text-accent-green font-medium">payroll </span>
           <span class="text-accent-brown font-medium">easily</span> in
           <span class="text-accent-red font-medium">seconds</span>
         </h1>
@@ -90,4 +92,43 @@ const toggleMenuText = computed(() => {
       </div>
     </section>
   </main>
+  <section class="py-20 bg-light-grey bg-wave-mobile bg-no-repeat bg-cover lg:bg-wave-desktop px-3">
+    <div class="mb-8 text-center">
+      <h2 class="font-bold mb-2 font-serif text-dark-green text-3xl">For Individuals and Businesses</h2>
+      <p>Join 200+ businesses using Eazipay's easy solution.</p>
+    </div>
+    <div class="flex flex-col lg:flex-row my-8 gap-4 lg:gap-0 justify-center text-white relative">
+      <article class="p-8 bg-dark-green lg:shadow-card-sh rounded-2xl text-left">
+        <h3 class="font-bold font-serif mb-4 text-lg">Tamper-proof Payroll for Life</h3>
+        <p class=" font-normal mb-3 text-base">Your staff payroll history is kept in one place forever.</p>
+        <p class=" font-normal mb-3 text-base">No more excel sheet or manual records.</p>
+        <p class=" font-normal text-base">Download your payroll history anytime you need it.</p>
+      </article>
+      <article class="p-8 bg-dark-green lg:shadow-card-sh rounded-2xl text-left lg:absolute lg:top-0 lg:bottom-0 lg:left-1/4 lg:w-6/12">
+        <h3 class="font-bold font-serif mb-4 text-lg">All Payroll, Anytime Anywhere</h3>
+        <p class=" font-normal mb-3 text-base">Wherever you are, Eazipay has got you covered on ALL your Payroll tasks.</p>
+        <p class=" font-normal  text-base">
+          Whether it is Taxes, Pension insurances HMOs, trustfunds, Eazipay handle all your
+          compliances in one place and easily, in seconds!
+        </p>
+      </article>
+      <article class="p-8 bg-dark-green lg:shadow-card-sh rounded-2xl text-left lg:relative lg:left-0 ">
+        <h3 class="font-bold font-serif mb-4 text-lg">Payroll in Seconds</h3>
+        <p class=" font-normal text-base mb-3">Never again will you spend more than 2 minutes on payroll.</p>
+        <p class=" font-normal text-base mb-3">Just click on your staff and bulk-pay them at once.</p>
+        <p class=" font-normal text-base ">Payment is done permanently.</p>
+      </article>
+    </div>
+    <p class="text-center max-w-lg mx-auto">We are happy to answer your queries. Please, reach us at <span class="text-accent-red">hello@myeazipay.com</span> and expect our response shortly after.</p>
+  </section>
+  <section>
+    <div>
+  <h3>How Eazipay Works</h3>
+  <p>Get started in 3 simple steps.</p>
+    </div>
+  <div>
+    <div></div>
+    <div></div>
+  </div>
+  </section>
 </template>
