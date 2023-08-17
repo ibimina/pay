07ee0860a9889f20f4bd97acb069dee7d614931f
Ibimina hart <ibimina.c.hart@gmail.com>
2023-08-17 12:51:39 +0100

    docs: change assest name

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,30 @@ export const theme = {
       'dark-logo': "url('/img/logo-dark.png')",
       'open-menu': "url('/img/icons8-menu.svg')",
       'close-menu': "url('/img/icon-close.svg')",
-      'color-gradient':"url('/img/gradients.png')",
+      'color-gradient': "url('/img/gradients.png')",
+      'desktop-bg': "url('/img/hero-media-desktop.png')",
     },
     backgroundColor: {
       'nav-bg': 'rgba(17,69,59,.5)',
       'dark-green': '#11453b'
     },
     boxShadow: {
-      'register-sh': '0 8px 16px rgba(17,69,59,.2)'
+      'register-sh': '0 8px 16px rgba(17,69,59,.2)',
+      'app-sh': '0 20px 48px 0 hsla(0,0%,66.7%,.29)'
     },
     textColor: {
       'semi-darkgrey': '#515251',
-      'dark-green': '#11453b'
+      'dark-green': '#11453b',
+      'accent-green': 'hsla(168, 60%, 17%, 1)',
+      'accent-brown': 'hsla(46, 31%, 58%, 1)',
+      'accent-red': 'hsla(1, 79%, 61%, 1)',
+      'accent-black': 'hsla(120, 1%, 16%, 1)',
+      'accent-white': 'hsla(0, 0%, 100%, 1)',
+      'accent-grey': 'hsla(120, 1%, 32%, 1)',
     },
     borderColor: {
       'dark-green': '#11453b'
-    },
+    }
   }
 }
 export const plugins = [];
