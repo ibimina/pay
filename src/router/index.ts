cc210ef7a24ca6836a82c2534f0f99c3d3c7adc6
Ibimina hart <ibimina.c.hart@gmail.com>
2023-08-18 10:34:32 +0100

    docs: add dashboard component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
+import DashView from '@/views/DashView.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: DashView
+    }
   ]
 })
 
---
+++ src/views/DashView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <header class="flex items-center justify-between">
+      <div class="lg:hidden flex items-center gap-2">
+        <img src="" alt="avatar" />
+        <p>Hello Wasiu</p>
+      </div>
+      <div class="hidden lg:flex items-center gap-4 justify-between relative">
+        <img src="" alt="avatar" />
+        <div>
+          <p>Kalu Abasiama</p>
+          <p>Admin</p>
+        </div>
+        <img class="absolute bottom-0 right-0"  src="" alt="arrow" />
+      </div>
+      <img src="" alt="notification bell" />
+    </header>
+  </div>
+</template>
